c0261c303437c65738ee1834184f050d81d0e00a
William Lee <karlweiyang@gmail,com>
2019-05-25 12:20:12 +0100

    Updates

---
+++ precache-manifest.f6b0664155b95585278d497373b200ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad4e4640d3b1bf9c0dd81d7af9e2ad0",
+    "url": "/FCC-random-quote-generator/index.html"
+  },
+  {
+    "revision": "8ee52aaaed54ee818a6e",
+    "url": "/FCC-random-quote-generator/static/css/main.2d054003.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/FCC-random-quote-generator/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "8ee52aaaed54ee818a6e",
+    "url": "/FCC-random-quote-generator/static/js/main.aade605a.chunk.js"
+  },
+  {
+    "revision": "3db1ede0efa9debfa3a0",
+    "url": "/FCC-random-quote-generator/static/js/runtime~main.d74b4bdc.js"
+  },
+  {
+    "revision": "65074676b56bed23816f05aecd85e8c9",
+    "url": "/FCC-random-quote-generator/static/media/quote.65074676.png"
+  },
+  {
+    "revision": "f29106f6f82f24c14018283fd2e3a917",
+    "url": "/FCC-random-quote-generator/static/media/twitter.f29106f6.png"
+  }
+]);
